79dca7992431c38fd2bec4944bb48fd2342e367e
Keyur Raval <keyur@getmagnet.com>
2015-04-20 15:58:49 +0530

    Changes for configuration

--- satellizer.js
+++ satellizer.js
@@ -10,10 +10,10 @@
     .constant('satellizer.config', {
       baseUrl: '/',
       httpInterceptor: true,
-      loginOnSignup: true,
-      loginRedirect: '/',
+      loginOnSignup: false,
+      loginRedirect: '/login',
       logoutRedirect: '/',
-      signupRedirect: '/login',
+      signupRedirect: '/signup',
       loginUrl: '/auth/login',
       signupUrl: '/auth/signup',
       loginRoute: '/login',
@@ -317,11 +317,11 @@
 
           storage.set(tokenName, token);
 
-          if (config.loginRedirect && !redirect) {
+          /*if (config.loginRedirect && !redirect) {
             $location.path(config.loginRedirect);
           }  else if (redirect && angular.isString(redirect)) {
             $location.path(encodeURI(redirect));
-          }
+          }*/
         };
 
         shared.removeToken = function() {
