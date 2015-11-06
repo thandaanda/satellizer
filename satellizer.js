a20dd5b0c95b7e3962ceebb4673b364d97ca830d
Keyur <keyur@getmagnet.com>
2015-11-06 17:44:30 +0530

    reasking permission

--- satellizer.js
+++ satellizer.js
@@ -3,6 +3,7 @@
  * (c) 2015 Sahat Yalkabov
  * License: MIT
  */
+<<<<<<< HEAD
 
 // CommonJS package manager support.
 if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports) {
@@ -157,6 +158,132 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
               config.httpInterceptor = function() {
                 return value;
               };
+=======
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular.module('satellizer', [])
+        .constant('satellizer.config', {
+            baseUrl: '/',
+            httpInterceptor: true,
+            loginOnSignup: false,
+            loginRedirect: '/login',
+            logoutRedirect: '/',
+            signupRedirect: '/signup',
+            loginUrl: '/auth/login',
+            signupUrl: '/auth/signup',
+            loginRoute: '/login',
+            signupRoute: '/signup',
+            tokenRoot: false,
+            tokenName: 'token',
+            tokenPrefix: 'satellizer',
+            unlinkUrl: '/auth/unlink/',
+            unlinkMethod: 'get',
+            authHeader: 'Authorization',
+            authToken: 'Bearer',
+            withCredentials: true,
+            platform: 'browser',
+            storage: 'localStorage',
+            providers: {
+                google: {
+                    name: 'google',
+                    url: '/auth/google',
+                    authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+                    redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+                    scope: ['profile', 'email'],
+                    scopePrefix: 'openid',
+                    scopeDelimiter: ' ',
+                    requiredUrlParams: ['scope'],
+                    optionalUrlParams: ['display'],
+                    display: 'popup',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 452,
+                        height: 633
+                    }
+                },
+                facebook: {
+                    name: 'facebook',
+                    url: '/auth/facebook',
+                    authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+                    redirectUri: window.location.origin + '/' || window.location.protocol + '//' + window.location.host + '/',
+                    scope: ['email','id','name','email','first_name','last_name'],
+                    scopeDelimiter: ',',
+                    requiredUrlParams: ['display', 'scope'],
+                    display: 'popup',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 580,
+                        height: 400
+                    }
+                },
+                linkedin: {
+                    name: 'linkedin',
+                    url: '/auth/linkedin',
+                    authorizationEndpoint: 'https://www.linkedin.com/uas/oauth2/authorization',
+                    redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+                    requiredUrlParams: ['state'],
+                    scope: ['r_emailaddress'],
+                    scopeDelimiter: ' ',
+                    state: 'STATE',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 527,
+                        height: 582
+                    }
+                },
+                github: {
+                    name: 'github',
+                    url: '/auth/github',
+                    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+                    redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+                    optionalUrlParams: ['scope'],
+                    scope: ['user:email'],
+                    scopeDelimiter: ' ',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 1020,
+                        height: 618
+                    }
+                },
+                yahoo: {
+                    name: 'yahoo',
+                    url: '/auth/yahoo',
+                    authorizationEndpoint: 'https://api.login.yahoo.com/oauth2/request_auth',
+                    redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+                    scope: [],
+                    scopeDelimiter: ',',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 559,
+                        height: 519
+                    }
+                },
+                twitter: {
+                    name: 'twitter',
+                    url: '/auth/twitter',
+                    type: '1.0',
+                    popupOptions: {
+                        width: 495,
+                        height: 645
+                    }
+                },
+                live: {
+                    name: 'live',
+                    url: '/auth/live',
+                    authorizationEndpoint: 'https://login.live.com/oauth20_authorize.srf',
+                    redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+                    scope: ['wl.emails'],
+                    scopeDelimiter: ' ',
+                    requiredUrlParams: ['display', 'scope'],
+                    display: 'popup',
+                    type: '2.0',
+                    popupOptions: {
+                        width: 500,
+                        height: 560
+                    }
+                }
+>>>>>>> reasking permission
             }
           }
         },
